6d20da8146aabb488e53e478a0b00672ba5b45c0
Jason <jason.longenecker@newlighttechnologies.com>
2015-02-19 14:34:57 -0500

    Morning Greeting from English-to-Spanish
    This time I'm creating a new branch for Brad to approve.

--- uselessJSForTesting.js
+++ uselessJSForTesting.js
@@ -3,7 +3,7 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-	greeting = 'Good evening!';
+	greeting = '!Buenas Dias';
 }
 else if (hourNow > 12){
 	greeting = 'Buenas Tardes!';
